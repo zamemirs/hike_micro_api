97970e6088b1c20946b962f52cea15ec8742e83c
zameer <zamemirs@gmail.com>
2025-08-22 01:00:54 +0530

    added docker compose

---
+++ db/init.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS customerdb;
+
+USE customerdb;
+
+CREATE TABLE IF NOT EXISTS customers (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100)
+);
