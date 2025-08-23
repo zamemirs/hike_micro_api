bf7c2cfdcf66123676d0848e44bc1ad92cf0e872
zamemirs <zamemirs@gmail.com>
2025-08-24 01:29:19 +0530

    changed demoapplicationtest from main to test folder

--- backend/src/test/java/com/example/demo/DemoApplicationTests.java
+++ backend/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,7 +1,6 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
